c7a9c7405bec7bb3bc10cd3b44f5d48551ef4f9d
cere2393 <carlos.ramirez@tecnologicoliceocristiano.edu.ec>
2024-07-11 22:23:01 -0500

    EjercicioEnClasePromedio

---
+++ obtenerpromedio.py
@@ -0,0 +1,9 @@
+#Elaborar un algoritmo que solicite dos numeros y muestre el promedio de ambos
+
+n1= int (input("ingrese el numero 1 :"))
+n2 =int (input("ingrese el numero 2 :"))
+
+s= n1+n2 
+
+p = s/2
+print(p)
